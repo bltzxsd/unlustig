72963026bd34456175fa51319da14d9943a54a33
bltzxsd <blissardartinq@gmail.com>
2022-01-08 16:55:03 +0530

    move cli logic to Cli functions

--- src/utils/args.rs
+++ src/utils/args.rs
@@ -1,70 +1,94 @@
 use std::{
     fs::{File, OpenOptions},
-    path::Path,
+    path::{Path, PathBuf},
 };
 
 use anyhow::{Context, Result};
-use clap::Parser;
+use clap::{Parser, ValueHint};
 
-use crate::error::ErrorKind;
+// use crate::error::ErrorKind;
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[clap(author, version, about)]
 pub struct Cli {
     #[clap(short = 'T', long, help = "Your caption goes here.")]
-    text: Option<String>,
+    text: String,
 
-    #[clap(short = 'G', long, help = "Path to the GIF file (without quotation marks)")]
-    gif: Option<String>,
+    #[clap(
+        short = 'G',
+        long,
+        help = "Path to the GIF file (without quotation marks)",
+        parse(from_os_str),
+        value_hint = ValueHint::FilePath
+    )]
+    gif: PathBuf,
 
     #[clap(
         short = 'o',
         long,
+        help = "Set the location of the output file (On Windows: Pictures folder | On Unix: Current directory))",
+        parse(from_os_str),
+        value_hint = ValueHint::DirPath
+    )]
+    output: Option<PathBuf>,
+
+    #[clap(
+        short = 'n',
+        long,
         help = "Set the name of the output file (default: out.gif)"
     )]
-    output: Option<String>,
+    name: Option<String>,
 }
 
 impl Cli {
-    pub fn text(&self) -> Option<&String> {
-        self.text.as_ref()
+    pub fn name(&self) -> String {
+        match &self.name {
+            Some(string) => {
+                if !string.contains(".gif") {
+                    return format!("{}.gif", string);
+                }
+                string.to_string()
+            }
+            None => "out.gif".to_string(),
+        }
+    }
+    pub fn text(&self) -> &str {
+        self.text.trim()
     }
 
-    pub fn gif(&self) -> Result<File> {
-        if self.gif.is_none() {
-            return Err(anyhow::Error::new(ErrorKind::BadGIF));
-        }
+    pub fn gif(&self) -> File {
         OpenOptions::new()
             .read(true)
-            .open(self.gif.as_ref().expect("gif unavailable"))
+            .open(&self.gif)
             .context("could not read gif")
+            .unwrap()
     }
 
-    pub fn output(&self) -> Mode {
-        let (text, gif, output) = (
-            self.text.is_some(),
-            self.gif.is_some(),
-            self.output.is_some(),
-        );
-        if text && gif && !output {
-            // output intentionally left out for default
-            Mode::Default
-        } else if !text && !gif && !output {
-            // nothing given, get all during runtime
-            Mode::InProgram
-        } else {
-            // all given
-            Mode::Cli(self.output.as_ref())
+    pub fn output(&self) -> PathBuf {
+        match &self.output {
+            Some(output) => output.to_path_buf(),
+            None => {
+                #[cfg(windows)]
+                return PathBuf::from(
+                    std::env::var("UserProfile").expect("no userprofile env key found"),
+                )
+                .join("Pictures");
+                #[cfg(unix)]
+                return PathBuf::from(
+                    std::env::current_dir()
+                        .expect("lacking permissions for current dir or curr dir is invalid"),
+                );
+            }
         }
     }
 }
 
-#[derive(Debug, Clone)]
-pub enum Mode<'m> {
-    Cli(Option<&'m String>),
-    InProgram,
-    Default,
-}
+// #[derive(Debug, Clone)]
+// pub enum Mode<'m> {
+//     Cli(Option<&'m String>),
+//     InProgram,
+//     Default,
+// }
 
 pub(crate) trait ToFile {
     fn to_file(&self) -> Result<File, std::io::Error>;
