a68a1a5f0d9544ab166535b0a78c63b328eec3e5
bltzxsd <blissardartinq@gmail.com>
2022-01-26 14:05:34 +0530

    added docs

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,13 +1,45 @@
-use std::{fs::File, io::Write, path::PathBuf};
+//! Helper utilities for other media processing activities.
+//!
+//! The `crate::utils` module contains common functions, and enums.
+
+use std::{fs::File, io::Write, iter, path::PathBuf};
 
 use log::{info, warn};
+use rand::{distributions::Alphanumeric, thread_rng, Rng};
 
+/// Argument handling with [`Clap`].
+///
+/// [`Clap`]: clap
 pub mod args;
+/// Gif captioning.
 pub mod gif;
+/// Caption creation.
 pub mod image;
+/// Video captioning.
 pub mod video;
 
-// Writes programs to the appdata folder on windows
+/// Contains the types of media supported by the application.
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub enum MediaType {
+    /// `.mp4` files.
+    Mp4,
+    /// `.avi` files.
+    Avi,
+    /// `.mkv` files.
+    Mkv,
+    /// `.webm` files.
+    Webm,
+    /// `.gif` files
+    Gif,
+}
+
+/// Writes [`Gifsicle`] and [`FFmpeg`] to the appdata folder on Windows.
+///
+/// # Result
+/// Returns an error if the `%appdata%` variable does not exist.
+///
+/// [`Gifsicle`]: https://www.lcdf.org/gifsicle/
+/// [`FFmpeg`]: https://www.ffmpeg.org/
 pub fn appdata_init() -> anyhow::Result<()> {
     #[cfg(windows)]
     let gifsicle = include_bytes!("../../deps/gifsicle/gifsicle.exe");
@@ -15,6 +47,7 @@ pub fn appdata_init() -> anyhow::Result<()> {
 
     let unlustig = PathBuf::from(std::env::var("APPDATA")?).join("unlustig-rs");
     // if appdata/unlustig-rs doesnt exist, we make a new one and write it
+
     if !unlustig.exists() {
         warn!("{} does not exist. Trying to create..", unlustig.display());
         std::fs::create_dir(&unlustig)?;
@@ -33,4 +66,14 @@ pub fn appdata_init() -> anyhow::Result<()> {
     }
 
     Ok(())
+}
+
+/// Generates a random name with 5 alphanumeric chars.
+pub fn random_name() -> String {
+    let mut rng = thread_rng();
+    iter::repeat(())
+        .map(|()| rng.sample(Alphanumeric))
+        .map(char::from)
+        .take(5)
+        .collect()
 }
