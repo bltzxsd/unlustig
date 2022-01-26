73ff824d4fa828ba7235af3412871dfd3d97d962
bltzxsd <blissardartinq@gmail.com>
2022-01-26 14:07:10 +0530

    added docs + moved function to mod.rs

--- src/utils/image.rs
+++ src/utils/image.rs
@@ -1,19 +1,28 @@
-use std::iter;
-
 use anyhow::Result;
 use image::{GenericImage, GenericImageView, ImageBuffer, Pixel, Primitive, Rgba, RgbaImage};
 use imageproc::drawing::{draw_text_mut, text_size};
-use rand::{distributions::Alphanumeric, thread_rng, Rng};
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use rusttype::{Font, Scale};
 
+use crate::error::ErrorKind;
+
+/// Holds the basic requirements to create a caption image.
+#[derive(Debug, Clone)]
 pub struct SetUp {
+    /// [`Font`] to be used.
     font: Font<'static>,
+    /// [`Scale`] of the text.
     scale: Scale,
+    /// Width of the input media.
     gif_w: u32,
 }
 
 impl SetUp {
+    /// Initialize the setup to create a caption image.
+    ///
+    /// This function *must* be followed by the [`with_dimensions()`] functions.
+    ///
+    /// [`with_dimensions()`]: crate::utils::image::SetUp::with_dimensions()
     pub const fn init(font: Font<'static>) -> Self {
         Self {
             font,
@@ -22,6 +31,9 @@ impl SetUp {
         }
     }
 
+    /// Adds the input media's dimensions to the struct.
+    ///
+    /// This function is *must* be used if contructing [`SetUp`]
     pub fn with_dimensions(self, width: u32, height: u32) -> Self {
         Self {
             gif_w: width,
@@ -30,21 +42,26 @@ impl SetUp {
         }
     }
 
+    /// Returns a reference to the [`Font`] of the image.
     pub const fn font(&self) -> &Font<'_> {
         &self.font
     }
 
+    /// Returns the [`Scale`] of the text.
     pub const fn scale(&self) -> Scale {
         self.scale
     }
 }
 
+/// Text Image is the second building block of an image caption.
+#[derive(Debug, Clone)]
 pub struct TextImage {
     init: SetUp,
     text: Vec<String>,
 }
 
 impl TextImage {
+    /// Create a new [`TextImage`] to be used to image captioning.
     pub fn new(init: SetUp, text: &str) -> Self {
         let text_nlsplit: Vec<_> = text.split('\n').collect();
         let split_texts: Vec<Vec<_>> = text_nlsplit
@@ -62,18 +79,31 @@ impl TextImage {
         Self { init, text }
     }
 
+    /// Render a [`TextImage`] into an image caption.
+    ///
+    /// Returns a correctly scaled [`ImageBuffer`] of the caption.
+    ///
+    /// # Errors
+    /// This function will return an error if the caption text
+    /// spans multiple lines and cannot be concatenated into
+    /// a single image.
+    /// See also: [`v_concat()`]
+    ///
+    /// [`v_concat()`]: crate::utils::image::TextImage::v_concat()
     pub fn render(self) -> Result<ImageBuffer<Rgba<u8>, Vec<u8>>> {
         let single = self.text.len() == 1;
-
+        let height = self.max_height()?;
         let image = if single {
             // this is fine because there is only one element
             // and so we do not need to concatenate images.
-            self.render_text(&self.text[0], single)
+            self.render_text(&self.text[0], height, single)
         } else {
             let images: Vec<_> = self
                 .text
                 .par_iter()
-                .map(|text| self.render_text(text, single))
+                .map(|text| {
+                    self.render_text(text, height, single)
+                })
                 .collect();
             Self::v_concat(&images)?
         };
@@ -83,12 +113,15 @@ impl TextImage {
         Ok(Self::resize(&image, self.init.gif_w, image_h as _))
     }
 
-    fn render_text(&self, text: &str, single: bool) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+    /// Renders a single line of text.
+    ///
+    /// Returns a transparent [`ImageBuffer`] with one line of caption
+    /// drawn.  
+    fn render_text(&self, text: &str, height: i32, single: bool) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
         let (text_width, text_height) = text_size(self.init.scale(), self.init.font(), text);
         // padding for the text up and down
-        let scale = if single { 2.5 } else { 1.3 };
-        let y_extension = (text_height as f32 * scale) as u32;
-        let mut image = RgbaImage::new(text_width as u32, y_extension);
+        let height = (height as f32 * if single { 2.5 } else { 1.3 }) as u32;
+        let mut image = RgbaImage::new(text_width as u32, height as u32);
         let y_offset = (image.height() as i32 - text_height) / 2;
         draw_text_mut(
             &mut image,
@@ -102,6 +135,24 @@ impl TextImage {
         image
     }
 
+    /// Returns the maximum height of the rendered text.
+    fn max_height(&self) -> Result<i32> {
+        let dimensions = |txt| text_size(self.init.scale(), self.init.font(), txt);
+        let h: Vec<_> = self.text.iter().map(|t| dimensions(t).1).collect();
+        let max_height = match h.iter().max_by_key(|&&x| x) {
+            Some(val) => *val,
+            None => return Err(ErrorKind::NoTextGiven.into()),
+        };
+
+        Ok(max_height)
+    }
+
+    /// Resizes the supplied image to a given width.
+    ///
+    /// Captions cannot exceed the width of the input media, which is why
+    /// the caption image needs to be resized to fit the width accordingly.
+    ///
+    /// This operation preserves aspect ratio.
     fn resize(
         image: &ImageBuffer<Rgba<u8>, Vec<u8>>,
         t_width: u32,
@@ -122,6 +173,9 @@ impl TextImage {
         )
     }
 
+    /// Overlays the text image on white buffer.
+    ///
+    /// This caption text image is centered.
     fn set_bg(
         buffer: &ImageBuffer<Rgba<u8>, Vec<u8>>,
         gif_w: u32,
@@ -138,7 +192,11 @@ impl TextImage {
         bg
     }
 
-    fn v_concat<I, P, S>(images: &[I]) -> Result<ImageBuffer<P, Vec<S>>>
+    /// Concatenates a collection of images vertically.
+    ///
+    /// This allows the program to draw individual lines at a time
+    /// and stitch them together vertically.
+    pub fn v_concat<I, P, S>(images: &[I]) -> Result<ImageBuffer<P, Vec<S>>>
     where
         I: GenericImageView<Pixel = P>,
         P: Pixel<Subpixel = S> + 'static,
@@ -167,7 +225,30 @@ impl TextImage {
         Ok(imgbuf)
     }
 
-    fn sum_until_fit(
+    /// Wraps text in accodance to the input image width.
+    ///
+    /// Returns a `Vec<Vec<usize>>` with indexes of where the splits should occur.
+    /// This function can handle manual newlines when they are in separate vectors:
+    ///
+    /// ```no_run
+    /// let text = "insert here\nsomething generic";
+    /// ```
+    /// Split and collect at newline and words:
+    /// ```no_run
+    /// let split_texts: Vec<Vec<_>> = text
+    ///     .iter()
+    ///     .map(|str| str.split_whitespace().collect())
+    ///     .collect();
+    /// ```
+    /// This is now looks like this:
+    /// ```no_run
+    /// let text = vec![
+    ///     [  "insert", "here" ], // these two internal vectors are seperated due to a newline
+    ///     [  "something", "generic" ]
+    /// ];
+    /// ```
+    /// Now, `text` can be passed into the function to create indices where splits should occur.
+    pub fn sum_until_fit(
         scale: Scale,
         font: &Font,
         image_width: i32,
@@ -195,11 +276,18 @@ impl TextImage {
         split_at
     }
 
+    /// Helper function to return the npercent of the text to be resized.
     fn npercent(current_width: u32, target_width: u32) -> u32 {
         (current_width as f32 * (target_width as f32 / current_width as f32)) as u32
     }
 
-    fn wrap_text(splits: &[Vec<usize>], texts: &[Vec<&str>]) -> Vec<String> {
+    /// Splits the given texts into multiple [`String`]s.
+    ///
+    /// These strings are split according to the given splits.
+    /// See also: [`sum_until_fit()`]
+    ///
+    /// [`sum_until_fit()`]: crate::utils::image::TextImage::sum_until_fit()
+    pub fn wrap_text(splits: &[Vec<usize>], texts: &[Vec<&str>]) -> Vec<String> {
         let mut wrapped_strings = vec![];
         let mut already_checked = 0;
 
@@ -215,6 +303,7 @@ impl TextImage {
     }
 }
 
+/// Create a new white image buffer.
 fn blank_buffer_new(w: u32, h: u32) -> RgbaImage {
     // text with only one word on it, does not have enough padding to look good
     let scale_factor: f32 = 1.2;
@@ -227,13 +316,4 @@ fn blank_buffer_new(w: u32, h: u32) -> RgbaImage {
         px.0 = [255, 255, 255, 255];
     }
     image
-}
-
-pub fn random_name() -> String {
-    let mut rng = thread_rng();
-    iter::repeat(())
-        .map(|()| rng.sample(Alphanumeric))
-        .map(char::from)
-        .take(5)
-        .collect()
 }
