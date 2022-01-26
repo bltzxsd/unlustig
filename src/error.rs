c9c6fd72883d3a7c2ff0c45491b60f1074adc784
bltzxsd <blissardartinq@gmail.com>
2022-01-26 14:02:53 +0530

    added docs

--- src/error.rs
+++ src/error.rs
@@ -1,10 +1,14 @@
 use thiserror::Error;
 
+/// List of general categories defining types of error.
 #[derive(Error, Debug)]
 pub enum ErrorKind {
-    // The supplied file was not a gif
+    /// Media format is not supported.
     #[error("unsupported media format: {0}")]
     UnsupportedMediaFormat(String),
+    /// General IO errors.
     #[error(transparent)]
     Io(#[from] std::io::Error),
+    #[error("no text was given")]
+    NoTextGiven,
 }
