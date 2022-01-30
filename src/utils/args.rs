af8bb3d769b77197d6029b7355d2a6dfb699d76a
bltzxsd <blissardartinq@gmail.com>
2022-01-30 23:33:04 +0530

    update help

--- src/utils/args.rs
+++ src/utils/args.rs
@@ -9,8 +9,15 @@ use std::{io, path::PathBuf};
 pub struct Cli {
     /// Caption for the image.
     ///
-    /// See also: [`Cli::text()`]
-    #[clap(short = 'T', long, help = "Your caption goes here.", required = true)]
+    /// See also: [`Cli::text()`]   
+    #[clap(
+        short = 'T',
+        long,
+        value_name = "Text",
+        help = "Your caption goes here.",
+        long_help = None,
+        required = true,
+    )]
     caption: String,
 
     /// Input media for processing.
@@ -21,7 +28,9 @@ pub struct Cli {
         long,
         help = "Path to the media file.",
         parse(from_os_str),
+        value_name = "Media: .mp4 / .gif etc.",
         value_hint = ValueHint::FilePath,
+        long_help = None,
         required = true
     )]
     media: PathBuf,
@@ -32,8 +41,9 @@ pub struct Cli {
     #[clap(
         short = 'o',
         long,
-        help = "Set the location of the output file.\n\nDefaults:\n\tOn Windows: User\\Pictures\\\n\tOn Unix   : Current directory",
-        value_name = "Output Directory",
+        help = "Set the location of the output file.\n\nDefaults:\n\tOn Windows: <drive>:\\Users\\<username>\\Pictures\\\n\tOn Unix: /home/<username>/Pictures",
+        long_help = None,
+        value_name = "Directory", 
         parse(from_os_str),
         value_hint = ValueHint::DirPath
     )]
@@ -45,7 +55,9 @@ pub struct Cli {
     #[clap(
         short = 'n',
         long,
-        help = "Set the name of the output file.\n\nDefault: generates a random alphanumeric name"
+        value_name = "Name",
+        help = "Set the name of the output file. Generates a random alphanumeric name if none given.",
+        long_help = None,
     )]
     output_name: Option<String>,
 
@@ -56,7 +68,8 @@ pub struct Cli {
     #[clap(
         short = 'f',
         long,
-        help = "Force overwrite the output file if one already exists.\n\nNote: if the output and input videos have the same name,\nthe input will not be overwritten."
+        help = "Force overwrite the output file if one already exists.",
+        long_help = "Force overwrite the output file if one already exists. Note: if the output and input videos have the same name, the input will not be overwritten"
     )]
     force_overwrite: bool,
 
@@ -68,7 +81,9 @@ pub struct Cli {
     #[clap(
         short = 'z',
         long,
-        help = "Optimize the output GIF.\n\nNote: Compression and processing time increases with higher values.\nPowered by Gifsicle (https://github.com/kohler/gifsicle) much <3",
+        value_name = "Level",
+        help = "Optimize the output GIF.",
+        long_help = "Optimize the output GIF. Note: Compression and processing time increases with higher values. Powered by Gifsicle",
         possible_values = ["O1", "O2", "O3"],
     )]
     optimization: Option<String>,
@@ -82,7 +97,9 @@ pub struct Cli {
     #[clap(
         short = 'l',
         long,
-        help = "Determines how lossy you want the GIF to be.\n\nHigher values result in smaller file sizes.\nPowered by Gifsicle",
+        value_name = "Level",
+        help = "Determines how lossy you want the GIF to be. Higher values result in smaller file sizes.",
+        long_help = None,
         possible_values = ["20", "40", "60", "80"],
     )]
     lossy: Option<u32>,
@@ -96,7 +113,8 @@ pub struct Cli {
     #[clap(
         short = 'r',
         long,
-        help = "Reduce the number of distinct colors in each output GIF.\nPowered by Gifsicle"
+        help = "Reduce the number of distinct colors in each output GIF. Powered by Gifsicle",
+        long_help = None,
     )]
     reduce: bool,
 }
@@ -168,7 +186,7 @@ impl Cli {
     ///
     /// # Result
     /// Returns an [`NotFound`] error if the default pictures folder was not found.
-    /// 
+    ///
     /// [`NotFound`]: std::io::ErrorKind::NotFound  
     pub fn output(&self) -> Result<PathBuf> {
         match &self.output_directory {
