2d231e5132a2c66b138b28e3289c44adcd6b39cf
bltzxsd <blissardartinq@gmail.com>
2022-03-22 18:56:10 +0530

    add update checking

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,10 @@ use log::{error, warn};
 
 use rich_presence::Discord;
 use rusttype::Font;
+use semver::Version;
+use serde_json::Value;
 use utils::{args::Cli, gif::process_gif, video::FFmpeg, MediaType};
+use yansi::Paint;
 
 /// Error module.
 pub(crate) mod error;
@@ -29,6 +32,9 @@ mod utils;
 fn main() {
     pretty_env_logger::init();
 
+    if let Err(e) = check_updates() {
+        warn!("failure checking updates: {e}");
+    }
     if let Err(e) = Discord::init(include_str!("RPC_ID")) {
         warn!("failed discord RPC initialization: {}", e);
     };
@@ -98,6 +104,28 @@ fn run(cli: &Cli) -> Result<()> {
     std::process::Command::new("xdg-open")
         .arg(out_path)
         .spawn()?;
+
+    Ok(())
+}
+
+fn check_updates() -> Result<()> {
+    let url = "https://api.github.com/repos/bltzxsd/unlustig/releases";
+    let request = ureq::get(url).call()?;
+    let data = request.into_string()?;
+    let git_tag = serde_json::from_str::<Value>(&data)?[0]["tag_name"]
+        .to_string()
+        .replace(&['\"', 'v'][..], "");
+    let (curr_ver, web_ver): (Version, Version) =
+        (clap::crate_version!().parse()?, git_tag.parse()?);
+    if curr_ver < web_ver {
+        warn!(
+            "{}\n{}",
+            Paint::red("unlustig is out of date!").bold(),
+            "Update here: https://github.com/bltzxsd/unlustig/releases/latest"
+        )
+    } else if curr_ver > web_ver {
+        warn!(":face_with_raised_eyebrow:")
+    }
 
     Ok(())
 }
