c4f8cc5abb10ed0fb77ada3392a01dba9ef88db7
bltzxsd <blissardartinq@gmail.com>
2022-04-26 17:40:14 +0530

    update documentation

--- src/error.rs
+++ src/error.rs
@@ -12,15 +12,14 @@ pub enum ErrorKind {
     Io(#[from] std::io::Error),
 
     /// No caption was provided.
-    /// This should never occur because the
-    /// caption is a required field.
+    ///
+    /// This error is only returned in the CLI mode
+    /// as required fields are forced to be filled
+    /// before any processing in the GUI.
     #[error("no text was given")]
     NoTextGiven,
 
     /// Gifsicle was not found.
-    /// Since unlustig cannot install its
-    /// dependencies on unix, users need to
-    /// install it themselves.
     #[error(
         "gifsicle not found, if using Unix, please install Gifsicle using your pkg manager: {0}"
     )]
@@ -28,9 +27,6 @@ pub enum ErrorKind {
     GifsicleNotFound(#[source] which::Error),
 
     /// FFmpeg was not found.
-    /// Since unlustig cannot install its
-    /// dependencies on unix, users need to
-    /// install it themselves.
     #[error("FFmpeg not found, if using Unix, please install FFmpeg using your pkg manager: {0}")]
     #[cfg(unix)]
     FfmpegNotFound(#[source] which::Error),
